66bb7831136e41ee80c78dba219b91e2bb5fcea5
gremliiin <rekufo@gmail.com>
2020-09-20 22:08:52 +0600

    Updates

---
+++ precache-manifest.2dcdd875f45ced9e131a8c7d0e72473d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb49bee6060d40ef246f6eace226cfbd",
+    "url": "/Nasa/index.html"
+  },
+  {
+    "revision": "7b3fac4d85c563829551",
+    "url": "/Nasa/static/css/main.85f59fc6.chunk.css"
+  },
+  {
+    "revision": "c017bf9a84501f784a89",
+    "url": "/Nasa/static/js/2.ace0acdd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Nasa/static/js/2.ace0acdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b3fac4d85c563829551",
+    "url": "/Nasa/static/js/main.814dde54.chunk.js"
+  },
+  {
+    "revision": "0646b0f359c14aa878e5",
+    "url": "/Nasa/static/js/runtime-main.22c146c1.js"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/Nasa/static/media/KumbhSans-Regular.d41d8cd9.ttf"
+  },
+  {
+    "revision": "f5561c3c87f91d619d0a07bede289a12",
+    "url": "/Nasa/static/media/cancel.f5561c3c.svg"
+  },
+  {
+    "revision": "23dea4621555e3e4f62796e88ecae153",
+    "url": "/Nasa/static/media/left.23dea462.svg"
+  },
+  {
+    "revision": "2528d18a396b8cc9393412cd47864773",
+    "url": "/Nasa/static/media/logo.2528d18a.png"
+  },
+  {
+    "revision": "b781c2d2de7120a3bd622b43307d828f",
+    "url": "/Nasa/static/media/preloader.b781c2d2.gif"
+  },
+  {
+    "revision": "49b9b4e110578776c4f5fb1ffcc31df5",
+    "url": "/Nasa/static/media/right.49b9b4e1.svg"
+  }
+]);
